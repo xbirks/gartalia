d2f9cc5b0a7f0ea3522fde2b0f3fc583119b2562
Andrés Ortega <andres03ortega@gmail.com>
2024-08-05 12:00:25 +0200

    gestión de imagenes heic

--- pages/api/sentEmail.js
+++ pages/api/sentEmail.js
@@ -1,3 +1,4 @@
+import sharp from 'sharp';
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
@@ -18,17 +19,42 @@ export default async function handler(req, res) {
         }
     });
 
-    const attachments = images.map((image, index) => {
+    // const attachments = images.map((image, index) => {
+    //     const matches = image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    //     if (!matches || matches.length !== 3) {
+    //         throw new Error("Invalid input string for image index " + index);
+    //     }
+    //     return {
+    //         filename: `Image${index + 1}.${matches[1].split('/')[1]}`, // Dynamic filename with extension
+    //         content: matches[2], // Base64 encoded content
+    //         encoding: 'base64'
+    //     };
+    // });
+
+    const attachments = await Promise.all(images.map(async (image, index) => {
         const matches = image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-        if (!matches || matches.length !== 3) {
-            throw new Error("Invalid input string for image index " + index);
+        if (!matches) {
+            console.error("Invalid image data for image index " + index);
+            return null; // Continuar con el siguiente archivo.
         }
+
+        let buffer = Buffer.from(matches[2], 'base64');
+        const imageType = matches[1].split('/')[1].toLowerCase(); // to handle 'jpeg', 'jpg' or 'png'
+
+        // Convertir HEIC a JPEG y reducir tamaño
+        if (imageType === 'heic' || imageType === 'jpeg' || imageType === 'jpg' || imageType === 'png') {
+            buffer = await sharp(buffer)
+              .resize(1024) // Cambia el ancho a 1024px, ajustando la altura para mantener la proporción.
+              .jpeg({ quality: 80 }) // Cambiar la calidad a 80 si es JPEG.
+              .toBuffer();
+        }
+
         return {
-            filename: `Image${index + 1}.${matches[1].split('/')[1]}`, // Dynamic filename with extension
-            content: matches[2], // Base64 encoded content
+            filename: `Image${index + 1}.${imageType === 'heic' ? 'jpeg' : imageType}`,
+            content: buffer,
             encoding: 'base64'
         };
-    });
+    }).filter(x => x)); // Filtra cualquier null que pueda haber sido añadido.
 
     const mailOptions = {
         from: process.env.SMTP_USER,
