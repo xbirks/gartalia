487e23b5c334450f22c51f7446c6d1bf43e376b2
Andrés Ortega <andres03ortega@gmail.com>
2025-03-10 22:17:08 +0100

    correcion errores mobile

--- app/poda-tala/components-poda-tala/cabecera/introPoda.jsx
+++ app/poda-tala/components-poda-tala/cabecera/introPoda.jsx
@@ -39,7 +39,6 @@ function Intro({ municipio }) {
 
       <h2 className="second_h2">Poda de árboles profesional para particulares y empresas</h2>
       <p className="second_p">La poda adecuada de los árboles es fundamental <strong>para su crecimiento saludable</strong> y la seguridad de su entorno, tanto en jardines privados como en espacios públicos. Con más de 20 años de experiencia, ofrecemos un servicio especializado en poda de árboles, incluyendo <strong>olivos, pinos, encinas, cipreses y algarrobos</strong>, especies típicas del Mediterráneo español. Nuestro equipo cualificado utiliza técnicas avanzadas y herramientas modernas <strong>para garantizar cortes precisos</strong> que favorezcan la regeneración del árbol y minimicen riesgos. Confíe en nosotros para mantener sus árboles fuertes y en perfecto estado.</p>
-```
     </div>
   );
 }
